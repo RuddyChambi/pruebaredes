ecf1a38cdc526fb4ae2f54d0e3c5da5657a34043
Ruddy Chambi <kael.ch2@gmail.com>
2024-08-27 09:32:38 -0400

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,7 +1,7 @@
 <?php
     define("SERVER","localhost");
     define("USER","root");
-    define("PASSWORD","");
+    define("PASSWORD","redes2024");
     define("DATABASE","notas_master");
     define("SET_NAMES","utf8");
 ?>
